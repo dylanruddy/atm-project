0a2eb34d0fba7aea37b4d347a9f1177feea990b3
dylanruddy <95940291+dylanruddy@users.noreply.github.com>
2022-04-15 22:43:08 -0400

    Checking & Savings accounts created.

--- atm.cpp
+++ atm.cpp
@@ -1,13 +1,17 @@
+//THIS PROGRAM ALLOWS THE USER TO ACCESS, CHECK BALANCE, DEPOSIT, AND MAKE WITHDRAWLS BETWEEN THEIR CHECKING & SAVINGS ACCOUNT
+//CREATED BY DYLAN JAMES RUDDY
+
 #include <iostream>
 
 using namespace std;
 
 void showmenu(){
     cout << "*****MENU*****" << endl;
-    cout << "1. Check balance" << endl;
-    cout << "2. Deposit" << endl;
-    cout << "3. Withdraw" << endl;
-    cout << "4. Cancel" << endl;
+    cout << "1. Checking Account Balance" << endl;
+    cout << "2. Savings Account Balance" << endl;
+    cout << "3. Deposit" << endl;
+    cout << "4. Withdraw" << endl;
+    cout << "5. Cancel" << endl;
     cout << "**************" << endl;
 }
 
@@ -16,7 +20,10 @@ int main(){
 
   int option;
   int password;
-  double balance = 500;
+  int account_selection;
+  int account_selection_withdrawl;
+  double checking_balance = 100;
+  double savings_balance = 1000;
   double deposit;
   double withdrawl;
   int withdrawl_confirmation;
@@ -28,7 +35,7 @@ int main(){
   cout << "Please enter your password:" << endl;
   cin >> password;
        if (password == 123){
-         cout << "Welcome" << endl;
+         cout << "Welcome User" << endl;
        }
 
        while (password != 123){
@@ -42,46 +49,87 @@ int main(){
   showmenu();
   cout << "Option: ";
   cin >> option; 
+
+//CHECKING ACCOUNT BALANCE
+
       if(option == 1){
-        cout << "Your balance is " << balance << endl;
+        cout << "Your checking account balance is $" << checking_balance << endl;
           }
 
+// SAVINGS ACCOUNT BALANCE
+
       else if (option == 2){
-        cout << "Please enter the amount of money you would like to deposit:" << endl;
-        cin >> deposit;
-        cout << "Your avalible balance is now " << (balance + deposit) << endl;
-        balance = balance + deposit;
-         }
+        cout << "Your savings account balance is $" << savings_balance << endl;
+      }    
+
+
+//DEPOSIT
 
       else if (option == 3){
-        cout << "Please enter the amount you would like to withdrawl" << endl;
-        cin >> withdrawl;
-        cout << "There is a $3.25 transaction fee. Would you like to proceed? (1 = Yes, 2 = No)" << endl;
-        cin >> withdrawl_confirmation;
-             if (withdrawl_confirmation == 1){
-               cout << "You've withdrawn $" << withdrawl << ". Your remaining avalible balance is $" << ((balance - withdrawl) - withdrawl_fee) << endl;
-             }
-            balance = ((balance - withdrawl) - withdrawl_fee);
+        cout << "Select the account you would like to make the deposit into" << endl;
+        cout << "1. Checking Account" << endl;
+        cout << "2. Savings Account" << endl;
+        cin >> account_selection;
+          
+          if (account_selection == 1){
+            cout << "Please enter the amount of money you would like to deposit into your checking account:" << endl;
+            cin >> deposit;
+            cout << "Your checking account avalible balance is now $" << (checking_balance + deposit) << endl;
+            checking_balance = checking_balance + deposit;
+          }
+
+          else if (account_selection == 2){
+            cout << "Please enter the amount of money you would like to deposit into your savings account:" << endl;
+            cin >> deposit;
+            cout << "Your savings account avalible balance is now $" << (savings_balance + deposit) << endl;
+            savings_balance = savings_balance + deposit;
+          }
+      }
+
+//WITHDRAWL
+
+      else if (option == 4){
+        cout << "Which account would you like to withdrawl from?" << endl;
+        cout << "1. Checking Account" << endl;
+        cout << "2. Savings Account" << endl;
+        cin >> account_selection_withdrawl;
+
+          if(account_selection_withdrawl == 1){
+
+             cout << "Please enter the amount you would like to withdrawl" << endl;
+             cin >> withdrawl;
+             cout << "There is a $3.25 transaction fee. Would you like to proceed? (1 = Yes, 2 = No)" << endl;
+             cin >> withdrawl_confirmation;
+        
+               if (withdrawl_confirmation == 1){
+                 cout << "You've withdrawn $" << withdrawl << ". Your checking account remaining avalible balance is $" << ((checking_balance - withdrawl) - withdrawl_fee) << endl;
+
+                  checking_balance = ((checking_balance - withdrawl) - withdrawl_fee);
+               }
+              else if (withdrawl_confirmation == 2){
+                  cout << "Your transaction has been canceled" << endl;
+                  }
               }
-             else if (withdrawl_confirmation == 2){
+      
+             
+           else if(account_selection_withdrawl == 2){
+
+             cout << "Please enter the amount you would like to withdrawl" << endl;
+             cin >> withdrawl;
+             cout << "There is a $3.25 transaction fee. Would you like to proceed? (1 = Yes, 2 = No)" << endl;
+             cin >> withdrawl_confirmation;
+               if (withdrawl_confirmation == 1){
+               cout << "You've withdrawn $" << withdrawl << ". Your  savings account remaining avalible balance is $" << ((savings_balance - withdrawl) - withdrawl_fee) << endl;
+               }
+               savings_balance = ((savings_balance - withdrawl) - withdrawl_fee);
+               }
+               else if (withdrawl_confirmation == 2){
                cout << "Your transaction has been canceled" << endl;
               }
+
+      }
  }
- while (option != 4);
-  
-      
-    
 
-
-
-
-
-
-
-
-
-
-
-
+ while (option != 5);
 
 }
